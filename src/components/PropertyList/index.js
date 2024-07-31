efa013773f12e1e154540dec0f86cb9f40a531c1
tejaswini <tejaswinikoya@gmail.com>
2024-07-31 17:43:53 +0530

    Updated code

--- src/components/PropertyList/index.js
+++ src/components/PropertyList/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropertyCard from '../PropertyCard';
-
 import './index.css';
 
 const PropertyList = ({ addToCart }) => {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    fetch('/properties.json')
+      .then(response => response.json())
+      .then(data => setProperties(data))
+      .catch(error => console.error('Error fetching properties:', error));
+  }, []);
+
   return (
     <div className="property-list">
-      {properties.map(property => (
-        <PropertyCard key={property.id} property={property} addToCart={addToCart} />
-      ))}
+      {properties.length > 0 ? (
+        properties.map(property => (
+          <PropertyCard key={property.id} property={property} addToCart={addToCart} />
+        ))
+      ) : (
+        <p>Loading properties...</p>
+      )}
     </div>
   );
 };
